bacc59daddd3243f9a3411c80aa270e3da0e1aed
Soukumarya Datta <52076271+Soukumarya-Datta@users.noreply.github.com>
2024-11-17 23:26:13 +0530

    Time: 0 ms (100.00%), Space: 16.6 MB (25.33%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.py
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -1,5 +1,3 @@
 class Solution:
     def convertTemperature(self, c: float) -> List[float]:
-        f = c * 1.8 +32
-        k = c + 273.15
-        return list([k,f])
+        return list([c + 273.15,c * 1.8 +32])
