e9a2d48e130a6e633c5b244e8f5c169ca5dc11d9
Soukumarya Datta <52076271+Soukumarya-Datta@users.noreply.github.com>
2025-04-20 01:38:20 +0530

    Time: 4 ms (21.79%), Space: 18 MB (15.64%) - LeetHub

---
+++ 2785-semi-ordered-permutation/2785-semi-ordered-permutation.py
@@ -0,0 +1,5 @@
+class Solution:
+    def semiOrderedPermutation(self, nums: List[int]) -> int:
+        
+        mn, mx = nums.index(1) , nums.index(n:= len(nums))     
+        return mn - mx + n - 1 - (mn > mx)
