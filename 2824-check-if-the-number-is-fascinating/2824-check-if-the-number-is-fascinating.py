224e6569b270162c8ca2853475cc81481aabb624
Soukumarya Datta <52076271+Soukumarya-Datta@users.noreply.github.com>
2025-04-21 23:30:41 +0530

    Time: 0 ms (100%), Space: 18 MB (13.85%) - LeetHub

--- 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.py
+++ 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.py
@@ -1,14 +1,6 @@
-from collections import Counter
 class Solution:
     def isFascinating(self, n: int) -> bool:
-        s = str(n) + str(2 * n) + str(3 * n)
-        c = Counter()
-        for i in s:
-            if c[i] == 0:
-                c[i] = 1
-            else:
-                return False
-        print(len(c))
-        if(len(c) == 9 and '0' not in c.keys()):
+        s = str(n) + str(2*n) + str(3*n)
+        if len(set(s)) == 9 and len(s) == 9 and '0' not in s:
             return True
         return False
